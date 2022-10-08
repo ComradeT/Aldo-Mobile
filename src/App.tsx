f98199af702766947199a5df3a0eb2f90edd3049
Anatoly Pautov <anatoliy.pautov@purrweb.com>
2022-10-08 16:16:50 +0600

    add svgr

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+      }}>
+      <Text>Aldo App</Text>
+    </View>
+  );
+};
+
+export default App;
